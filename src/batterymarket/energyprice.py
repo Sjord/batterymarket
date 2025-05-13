54c6fabec94b9e63273205fa25d7a024efc7aa6e
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2025-05-13 15:18:13 +0200

    Cache response

--- src/batterymarket/energyprice.py
+++ src/batterymarket/energyprice.py
@@ -3,39 +3,51 @@ from datetime import datetime, timedelta, UTC
 from zoneinfo import ZoneInfo
 import json
 import subprocess
+import os
 
 tz_ams = ZoneInfo("Europe/Amsterdam")
 
 today = datetime.now(tz=tz_ams).replace(hour=0, minute=0, second=0, microsecond=0)
 tomorrow = today + timedelta(days=1)
 
-from_time = today.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
-to_time = tomorrow.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+date_str = today.strftime("%Y-%m-%d")
+cache_path = f"/Users/sjoerd/dev/batterymarket/prices/{date_str}.json"
 
-url = 'https://newtransparency.entsoe.eu/market/energyPrices/load'
-payload = {
-   "areaList": [
-      "BZN|10YNL----------L"
-   ],
-   "dateTimeRange": {
-      "from": from_time,
-      "to": to_time,
-   },
-   "filterMap": {},
-   "intervalPageInfo": {
-      "itemIndex": 0,
-      "pageSize": 10
-   },
-   "sorterList": [],
-   "timeZone": "CET"
-}
-headers = {
-    "Content-Type": "application/json; charset=utf-8",
-    "User-Agent": "https://github.com/Sjord/batterymarket",
-}
+if os.path.exists(cache_path):
+    with open(cache_path, "r") as f:
+        data = json.load(f)
+else:
+    from_time = today.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    to_time = tomorrow.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
-response = requests.post(url, json=payload, headers=headers)
-data = response.json()
+    url = 'https://newtransparency.entsoe.eu/market/energyPrices/load'
+    payload = {
+    "areaList": [
+        "BZN|10YNL----------L"
+    ],
+    "dateTimeRange": {
+        "from": from_time,
+        "to": to_time,
+    },
+    "filterMap": {},
+    "intervalPageInfo": {
+        "itemIndex": 0,
+        "pageSize": 10
+    },
+    "sorterList": [],
+    "timeZone": "CET"
+    }
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "User-Agent": "https://github.com/Sjord/batterymarket",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    with open(cache_path, "w") as f:
+        json.dump(data, f, indent=2)
+
 
 prices = data['instanceList'][0]['curveData']['periodList'][0]['pointMap']
 curhour = datetime.now(tz=tz_ams).hour
