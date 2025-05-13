aff9377dbec33ddc0d2f6132183813756d459519
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2025-05-13 15:03:01 +0200

    Use correct timezone

--- src/batterymarket/energyprice.py
+++ src/batterymarket/energyprice.py
@@ -1,29 +1,33 @@
-
 import requests
 from datetime import datetime, timedelta, UTC
+from zoneinfo import ZoneInfo
 import json
 import subprocess
 
-today = datetime.now(UTC).replace(hour=22, minute=0, second=0, microsecond=0)
+tz_ams = ZoneInfo("Europe/Amsterdam")
+
+today = datetime.now(tz=tz_ams).replace(hour=0, minute=0, second=0, microsecond=0)
 tomorrow = today + timedelta(days=1)
 
+from_time = today.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+to_time = tomorrow.astimezone(UTC).strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
 url = 'https://newtransparency.entsoe.eu/market/energyPrices/load'
 payload = {
-   "areaList" : [
+   "areaList": [
       "BZN|10YNL----------L"
    ],
-   "dateTimeRange" : {
-      "from" : today.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-      "to" : tomorrow.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+   "dateTimeRange": {
+      "from": from_time,
+      "to": to_time,
    },
-   "filterMap" : {},
-   "intervalPageInfo" : {
-      "itemIndex" : 0,
-      "pageSize" : 10
+   "filterMap": {},
+   "intervalPageInfo": {
+      "itemIndex": 0,
+      "pageSize": 10
    },
-   "sorterList" : [],
-   "timeZone" : "CET"
+   "sorterList": [],
+   "timeZone": "CET"
 }
 headers = {
     "Content-Type": "application/json; charset=utf-8",
@@ -32,15 +36,15 @@ headers = {
 
 response = requests.post(url, json=payload, headers=headers)
 data = response.json()
-# print(json.dumps(data))
-prices = data['instanceList'][0]['curveData']['periodList'][0]['pointMap']
-curhour = datetime.now().hour
-curprice = float(prices[str(curhour)][0])
 
+prices = data['instanceList'][0]['curveData']['periodList'][0]['pointMap']
+curhour = datetime.now(tz=tz_ams).hour
+curprice = float(prices[str(curhour)][0])
 pricelist = [float(p[0]) for p in prices.values()]
 maxprice = max(pricelist)
 minprice = min(pricelist)
 ratio = (curprice - minprice) / (maxprice - minprice)
 batterylevel = int(100 - 50 * ratio)
+
 print(f"hour: {curhour} price: {curprice}, min: {minprice}, max: {maxprice}, ratio: {ratio}, batterylevel: {batterylevel}")
 subprocess.run(['/usr/local/bin/bclm', 'write', str(batterylevel)])
